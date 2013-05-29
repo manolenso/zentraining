8c5516c5be71872ee6e5e8d6d82a1a2321180ed4
manolenso <rlaurent@tvmag.com>
2013-05-29 16:26:13 +0200

    first commit

---
+++ zencoding_training.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+
+<script type="text/javascript" src="zen_textarea.js"></script>
+<link rel="stylesheet" href="style.css" media="all">
+
+  <meta charset="UTF-8">
+
+  <title>Document</title>
+</head>
+
+<body>
+  <------------------code-------------->
+div#page>div.logo+ul#navigation>li*5>a
+
+<div id="page">
+    <div class="logo"></div>
+    <ul id="navigation">
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+    </ul>
+  </div>
+<------------------code-------------->
+html:5>div#page>div#header>ul.navigation>li*4>a
+
+<!doctype html>
+<html lang="en">
+<head>
+
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <div id="page">
+    <div id="header">
+      <ul class="navigation">
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+      </ul>
+    </div>
+  </div>
+</body>
+</html>
+<------------------code-------------->
+
+#menu>header>ul>li*5>a
+
+<div id="menu">
+  <header>
+    <ul>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+    </ul>
+  </header>
+</div>
+
+<------------------code-------------->
+
+#nav>ul>li*6>a>
+
+<div id="nav">
+  <ul>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+  </ul>
+</div>
+
+<------------------code-------------->
+
+html:5>.menu>ul>li*6>a
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <div class="menu">
+    <ul>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+    </ul>
+  </div>
+</body>
+</html>
+
+
+
+<-----------------code-------------->
+
+<------------------code-------------->
+
+<------------------code-------------->
+
+<------------------code-------------->
+
+</body>
+</html>
